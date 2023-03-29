2fd334e1eb25bbc31824b3e1a78dbb3f282874a8
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-29 09:38:56 +0200

    worked with Tim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Auth, createUserWithEmailAndPassword} from "@angular/fire/auth";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Auth';
+
+  auth: Auth = inject(Auth)
+
+  signUp(){
+    createUserWithEmailAndPassword(this.auth, "test@gmail.com", "test@gmail.com").then(console.log).catch(console.error)
+  }
 }
