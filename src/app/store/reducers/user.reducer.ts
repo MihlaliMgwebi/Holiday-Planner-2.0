52ecdfb8dcdbec1e33a0a8b6842c95c3a79cbfba
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-04-11 22:05:43 +0200

    feat: state creation and slicing -[X] Create state interface -[X] Create initial state -[X] Create reducer -[X] Create selectors

--- src/app/store/reducers/user.reducer.ts
+++ src/app/store/reducers/user.reducer.ts
@@ -1,18 +1,54 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
 import * as UserActions from '../actions/user.actions';
+import {User} from "../../models/user.model";
 
 export const userFeatureKey = 'user';
 
-export interface State {
-
+export interface UserState {
+  email: string,
+  password: string,
+  loggedInUser: User,
+  error: unknown
 }
 
-export const initialState: State = {
-
+export const initialState: UserState = {
+  email: '',
+  password: '',
+  loggedInUser: {
+    displayName: null,
+    email: null,
+    emailVerified: false,
+    photoURL: null,
+    uid: null,
+  },
+  error: null,
 };
 
 export const reducer = createReducer(
   initialState,
+  on(UserActions.setSignedUpNewUserComplete,(state) => (state)),
+  on(UserActions.setSignedInExistingUserComplete,(state, { email, password}) => ({...state, email, password})),
+  on(UserActions.setSignedInComplete, UserActions.setLoggedInUserOnBrowserReload, UserActions.setLoggedInUserOnBrowserReload,(state, {loggedInUser}) => ({...state,loggedInUser, error: null})),
+  on(UserActions.setSignedInFailure,(state, {error}) => (
+    {...state,
+      loggedInUser: {
+        displayName: null,
+        email: null,
+        emailVerified: false,
+        photoURL: null,
+        uid: null,
+      },
+      error
+    })),
+  on(UserActions.setSignedOutComplete,(state) => ({...state, email:'', password: '', loggedInUser: {
+      displayName: null,
+      email: null,
+      emailVerified: false,
+      photoURL: null,
+      uid: null,
+    },
+    error: null
+  })),
 );
 
 export const userFeature = createFeature({
--- src/app/store/selectors/user.selectors.ts
+++ src/app/store/selectors/user.selectors.ts
@@ -1,6 +1,19 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromUser from '../reducers/user.reducer';
 
-export const selectUserState = createFeatureSelector<fromUser.State>(
+export const selectUserState = createFeatureSelector<fromUser.UserState>(
   fromUser.userFeatureKey
 );
+
+export const selectLoggedInUser = createSelector(
+  selectUserState,
+  (state) => {
+    return state.loggedInUser
+  }
+)
+
+export const selectIsLoggedIn = createSelector(
+  selectLoggedInUser,
+  (loggedInUser) => !!(loggedInUser.uid) //convert a truthy or falsy value to a boolean.
+)
+
