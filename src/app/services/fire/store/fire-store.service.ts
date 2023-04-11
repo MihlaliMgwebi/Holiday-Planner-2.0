8740ed7345e668c6edd8453f3254dd4708680613
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-04-11 23:03:27 +0200

    feat: services and effects creation
    -[X] Create mock functions
    -[X] When action is dispatched, handle side effects

--- src/app/services/fire/store/fire-store.service.ts
+++ src/app/services/fire/store/fire-store.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Trip} from "../../../models/trip.model";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {ItineraryItem} from "../../../models/itineraryItem.model";
 
 @Injectable({
   providedIn: 'root'
@@ -19,18 +20,30 @@ export class FireStoreService {
     return this.http
       .post<Trip>(this.mockTripApiUrl, trip)
   }
-
+  createItineraryItem(itineraryItem: ItineraryItem) :  Observable<ItineraryItem> {
+    return this.http
+      .post<ItineraryItem>(this.mockItineraryItemApiUrl, itineraryItem)
+  }
   // READ
   getTrips() :  Observable<Trip[]> {
     return this.http
       .get<Trip[]>('/assets/trips.json')
   }
 
+  getAllItineraryItems() :  Observable<ItineraryItem[]> {
+    console.log('itineraryItems')
+    return this.http
+      .get<ItineraryItem[]>('/assets/itineraryItems.json')
+  }
 
   // UPSERT
   upsertTrip(trip: Trip) :  Observable<Trip> {
     return this.http
       .put<Trip>(this.mockTripApiUrl, trip)
+  }
+  upsertItineraryItem(itineraryItem: ItineraryItem) :  Observable<ItineraryItem> {
+    return this.http
+      .put<ItineraryItem>(this.mockItineraryItemApiUrl, itineraryItem)
   }
 
   // DELETE
