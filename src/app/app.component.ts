cb17affe2a3f4aaaf5ff1f42dbe4c4700a1ff5d4
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-04-11 21:58:54 +0200

    feat: services and effects setup -[X] Convert functions to return observables -[X] When action is dispatched, handle side effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AuthService} from "./services/fire/auth.service";
+import {AuthService} from "./services/fire/auth/auth.service";
 
 @Component({
   selector: 'app-root',
