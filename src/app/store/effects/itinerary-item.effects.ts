895798daf550db46aaa8062308da9e9dd3012a22
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-04-14 13:59:16 +0200

    feat: replace mock

--- src/app/store/effects/itinerary-item.effects.ts
+++ src/app/store/effects/itinerary-item.effects.ts
@@ -1,70 +1,87 @@
-import {Injectable} from '@angular/core';
-import {Actions, createEffect, ofType} from '@ngrx/effects';
-import * as ItineraryItemActions from "../actions/itinerary-item.actions";
-import {catchError, map, switchMap} from "rxjs/operators";
-import {of} from "rxjs";
-import {FireStoreService} from "../../services/fire/store/fire-store.service";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import * as ItineraryItemActions from '../actions/itinerary-item.actions';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { FireStoreService } from '../../services/fire/store/store.service';
 
 @Injectable()
 export class ItineraryItemEffects {
   // CREATE
-  createItineraryItem$ = createEffect(()=> {
+  createItineraryItem$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ItineraryItemActions.createItineraryItem),
-      switchMap(({itineraryItem}) =>
-        this.fireStoreService.createItineraryItem(itineraryItem)
-          .pipe(
-            map(res => ItineraryItemActions.createItineraryItemComplete({itineraryItem: res})),
-            catchError(error => of(ItineraryItemActions.createItineraryItemFailure({error})))
+      switchMap(({ itineraryItem }) =>
+        this.fireStoreService.createItineraryItem(itineraryItem).pipe(
+          map((res) =>
+            ItineraryItemActions.createItineraryItemComplete({
+              itineraryItem: res,
+            })
+          ),
+          catchError((error) =>
+            of(ItineraryItemActions.createItineraryItemFailure({ error }))
           )
+        )
       )
-    )
+    );
   });
 
   // READ
-  getAllItineraryItems$ = createEffect(()=> {
+  getAllItineraryItems$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ItineraryItemActions.getAllItineraryItems),
       switchMap(() => {
-          console.log('Tim ')
-          return this.fireStoreService.getAllItineraryItems()
-            .pipe(
-              map(res => ItineraryItemActions.getAllItineraryItemsComplete({allItineraryItems: res})),
-              catchError(error => of(ItineraryItemActions.getAllItineraryItemsFailure({error})))
-            )
-      }
-
-      )
-    )
+        console.log('Tim ');
+        return this.fireStoreService.getAllItineraryItems().pipe(
+          map((res) =>
+            ItineraryItemActions.getAllItineraryItemsComplete({
+              allItineraryItems: res,
+            })
+          ),
+          catchError((error) =>
+            of(ItineraryItemActions.getAllItineraryItemsFailure({ error }))
+          )
+        );
+      })
+    );
   });
 
   // UPDATE
-  upsertItineraryItem$ = createEffect(()=> {
+  upsertItineraryItem$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ItineraryItemActions.upsertItineraryItem),
-      switchMap(({upsertedItineraryItem}) =>
-        this.fireStoreService.upsertItineraryItem(upsertedItineraryItem)
-          .pipe(
-            map(res => ItineraryItemActions.upsertItineraryItemComplete({upsertedItineraryItem: res})),
-            catchError(error => of(ItineraryItemActions.upsertItineraryItemFailure({error})))
+      switchMap(({ upsertedItineraryItem }) =>
+        this.fireStoreService.upsertItineraryItem(upsertedItineraryItem).pipe(
+          map((res) =>
+            ItineraryItemActions.upsertItineraryItemComplete({
+              upsertedItineraryItem: res,
+            })
+          ),
+          catchError((error) =>
+            of(ItineraryItemActions.upsertItineraryItemFailure({ error }))
           )
+        )
       )
-    )
+    );
   });
 
   // DELETE
-  deleteItineraryItem$ = createEffect(()=> {
+  deleteItineraryItem$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ItineraryItemActions.deleteItineraryItem),
-      switchMap(({deletedItineraryItemId}) =>
-        this.fireStoreService.deleteItineraryItem(deletedItineraryItemId)
-          .pipe(
-            map(_ => ItineraryItemActions.deleteItineraryItemComplete()),
-            catchError(error => of(ItineraryItemActions.deleteItineraryItemFailure({error})))
+      switchMap(({ deletedItineraryItemId }) =>
+        this.fireStoreService.deleteItineraryItem(deletedItineraryItemId).pipe(
+          map((_) => ItineraryItemActions.deleteItineraryItemComplete()),
+          catchError((error) =>
+            of(ItineraryItemActions.deleteItineraryItemFailure({ error }))
           )
+        )
       )
-    )
+    );
   });
 
-  constructor(private actions$: Actions, private fireStoreService: FireStoreService) {}
+  constructor(
+    private actions$: Actions,
+    private fireStoreService: FireStoreService
+  ) {}
 }
--- src/app/store/effects/trip.effects.ts
+++ src/app/store/effects/trip.effects.ts
@@ -2,67 +2,66 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
-import * as TripActions from "../actions/trip.actions";
-import {FireStoreService} from "../../services/fire/store/fire-store.service";
+import * as TripActions from '../actions/trip.actions';
+import { FireStoreService } from '../../services/fire/store/store.service';
 @Injectable()
 export class TripEffects {
   // Trips and ItineraryItems are root collections in DB
 
   //CREATE
-  createTrip$ = createEffect(()=> {
+  createTrip$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TripActions.createTrip),
-      switchMap(({trip}) =>
-        this.fireStoreService.createTrip(trip)
-          .pipe(
-            map(res => TripActions.createTripComplete({trip: res})),
-            catchError(error => of(TripActions.createTripFailure({error})))
-          )
+      switchMap(({ trip }) =>
+        this.fireStoreService.createTrip(trip).pipe(
+          map((res) => TripActions.createTripComplete({ trip: res })),
+          catchError((error) => of(TripActions.createTripFailure({ error })))
+        )
       )
-    )
+    );
   });
 
   // READ
-  getAllTrips$ = createEffect(()=> {
+  getAllTrips$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TripActions.getAllTrips),
       switchMap(() =>
-        this.fireStoreService.getTrips()
-          .pipe(
-            map(res => TripActions.getAllTripsComplete({allTrips: res})),
-            catchError(error => of(TripActions.getAllTripsFailure({error})))
-          )
+        this.fireStoreService.getTrips().pipe(
+          map((res) => TripActions.getAllTripsComplete({ allTrips: res })),
+          catchError((error) => of(TripActions.getAllTripsFailure({ error })))
+        )
       )
-    )
+    );
   });
 
   // UPSERT
-  upsertTrip$ = createEffect(()=> {
+  upsertTrip$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TripActions.upsertTrip),
-      switchMap(({trip}) =>
-        this.fireStoreService.createTrip(trip)
-          .pipe(
-            map(res => TripActions.upsertTripComplete({upsertedTrip: res})),
-            catchError(error => of(TripActions.upsertTripFailure({error})))
-          )
+      switchMap(({ trip }) =>
+        this.fireStoreService.createTrip(trip).pipe(
+          map((res) => TripActions.upsertTripComplete({ upsertedTrip: res })),
+          catchError((error) => of(TripActions.upsertTripFailure({ error })))
+        )
       )
-    )
+    );
   });
 
   // DELETE
-  deleteTrip$ = createEffect(()=> {
+  deleteTrip$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TripActions.deleteTrip),
-      switchMap(({deletedTripId}) =>
-        this.fireStoreService.deleteTrip(deletedTripId)
-          .pipe(
-            map(_ => TripActions.deleteTripComplete()),
-            catchError(error => of(TripActions.deleteTripFailure({error})))
-          )
+      switchMap(({ deletedTripId }) =>
+        this.fireStoreService.deleteTrip(deletedTripId).pipe(
+          map((_) => TripActions.deleteTripComplete()),
+          catchError((error) => of(TripActions.deleteTripFailure({ error })))
+        )
       )
-    )
+    );
   });
 
-  constructor(private actions$: Actions, private fireStoreService: FireStoreService) {}
+  constructor(
+    private actions$: Actions,
+    private fireStoreService: FireStoreService
+  ) {}
 }
