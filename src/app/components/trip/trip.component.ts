233271fde36768a1c0e6f0e80707d41b57959b62
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-04-17 07:48:47 +0200

    fix:  reroute on refresh
    Thank you Charel

--- src/app/components/trip/trip.component.ts
+++ src/app/components/trip/trip.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Trip } from '../../models/trip.model';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TripState } from '../../store/reducers/trip.reducer';
 import { CorrelatedData } from '../../models/correlatedData.model';
 import { Store } from '@ngrx/store';
-import { deleteTrip, getAllTrips, setSelectedTrip } from '../../store/actions/trip.actions';
-import { getAllItineraryItems } from '../../store/actions/itinerary-item.actions';
 import { Observable } from 'rxjs';
-import {
-  selectCorrelatedTrips,
-  selectedCorrelatedData,
-  selectSelectedCorrelatedTrip,
-} from '../../store/selectors/trip.selectors';
-import { ItineraryItemState } from '../../store/reducers/itinerary-item.reducer';
+import { selectedCorrelatedData } from '../../store/selectors/trip.selectors';
 import { ActivatedRoute, Router } from '@angular/router';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-trip',
   templateUrl: './trip.component.html',
   styleUrls: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TripComponent {
   selectedCorrelatedData$: Observable<CorrelatedData | null>;
 
-  constructor(private tripStore: Store<TripState>) {
+  constructor(private tripStore: Store<TripState>, private route: ActivatedRoute, private router: Router) {
     this.selectedCorrelatedData$ = tripStore.select(selectedCorrelatedData);
+    this.selectedCorrelatedData$.subscribe((correlatedData) =>
+      !correlatedData ? this.router.navigate([`../../`], { relativeTo: this.route }) : ''
+    );
   }
 }
